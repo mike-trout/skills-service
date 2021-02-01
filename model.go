8e4b65d0365f4c49f497e78f8dad993d93be77a1
Mike Trout <mike.trout@gmail.com>
2021-02-01 13:41:58 +0000

    Update skills.

--- model.go
+++ model.go
@@ -15,18 +15,18 @@ type Skill struct {
 
 // Skills - global var containing all skills
 var Skills = []Skill{
-	Skill{ID: 1, Name: "Natural", Level: 10},
-	Skill{ID: 2, Name: "NaturalONE", Level: 10},
-	Skill{ID: 3, Name: "JavaScript", Level: 8},
-	Skill{ID: 4, Name: "Java", Level: 7},
-	Skill{ID: 5, Name: "C#", Level: 6},
-	Skill{ID: 6, Name: "Go", Level: 3},
-	Skill{ID: 7, Name: "HTML", Level: 8},
-	Skill{ID: 8, Name: "CSS", Level: 8},
-	Skill{ID: 9, Name: "React", Level: 5},
-	Skill{ID: 10, Name: "Docker", Level: 7},
-	Skill{ID: 11, Name: "Kubernetes", Level: 6},
-	Skill{ID: 12, Name: "AWS", Level: 5},
+	{ID: 1, Name: "Natural", Level: 10},
+	{ID: 2, Name: "NaturalONE", Level: 10},
+	{ID: 3, Name: "JavaScript", Level: 9},
+	{ID: 9, Name: "React", Level: 8},
+	{ID: 4, Name: "Java", Level: 7},
+	{ID: 5, Name: "C#", Level: 6},
+	{ID: 6, Name: "Go", Level: 3},
+	{ID: 7, Name: "HTML", Level: 8},
+	{ID: 8, Name: "CSS", Level: 8},
+	{ID: 12, Name: "AWS", Level: 6},
+	{ID: 11, Name: "Kubernetes", Level: 7},
+	{ID: 10, Name: "Docker", Level: 8},
 }
 
 func getSkills() ([]Skill, error) {
