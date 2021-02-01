f77c9f8ea4f2b8b91fc275bf8070d0abb55ee0ca
Mike Trout <mike.trout@gmail.com>
2021-02-01 13:41:07 +0000

    Update Travis pipeline.

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,10 @@
 set -e
 
 # Install gcloud
-curl https://sdk.cloud.google.com | bash > /dev/null
-
-# Promote gcloud to PATH top priority (prevent using old version from Travis)
-source $HOME/google-cloud-sdk/path.bash.inc
-
-# Make sure kubectl is updated to latest version
-gcloud components update kubectl
+sudo apt-get update -y && sudo apt-get install apt-transport-https ca-certificates gnupg -y
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt-get update -y && sudo apt-get install google-cloud-sdk kubectl -y
 
 gcloud auth activate-service-account --key-file gcloud-service-account-secret.json
 gcloud container clusters get-credentials gke-cluster --zone us-central1-a --project www-miketrout-dev
